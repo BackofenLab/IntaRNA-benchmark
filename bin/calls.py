a80e03b37eba6f6b4da1085d5a59c3b6d39ed783
RickGelhausen <rick.gelhausen@gmail.com>
2019-07-16 17:46:54 +0200

    fix for calls.py

--- bin/calls.py
+++ bin/calls.py
@@ -27,7 +27,7 @@ def runSubprocess(callArgs):
     try:
         output = check_output(callArgs,stderr=STDOUT).decode("utf-8")
     except CalledProcessError as e:
-        print("calling "+(" ".join(e.cmd))+" produced error code "+e.returncode+" and output "+e.output)
+        print("calling "+(" ".join(e.cmd))+" produced error code "+str(e.returncode)+" and output "+str(e.output))
     std_out = list(filter(None, output.replace("\t","").split("\n")))
     # create a dictionary containing output of /usr/bin/time -v
     stat_dict = dict()
